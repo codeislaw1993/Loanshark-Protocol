0b4d29b84d0492b515d6b751ef87002b33b5cb97
Jay Cheung Chun Man <Jay Cheung Chun Man@JAY>
2023-10-11 01:49:07 +0800

    initial commit

---
+++ contracts/MainDemoConsumerBase.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.4;
+
+import "./RedstoneConsumerNumericBase.sol";
+
+contract MainDemoConsumerBase is RedstoneConsumerNumericBase {
+  function getDataServiceId() public view virtual override returns (string memory) {
+    return "redstone-main-demo";
+  }
+
+  function getUniqueSignersThreshold() public view virtual override returns (uint8) {
+    return 1;
+  }
+
+  function getAuthorisedSignerIndex(address signerAddress)
+    public
+    view
+    virtual
+    override
+    returns (uint8)
+  {
+    if (signerAddress == 0x0C39486f770B26F5527BBBf942726537986Cd7eb) {
+      return 0;
+    } else {
+      revert SignerNotAuthorised(signerAddress);
+    }
+  }
+}
